f3683ebdcb805ece27da49147bcea8fb0873eea4
gustavo9601 <tavo9601@gmail.com>
2022-09-03 20:00:50 -0500

    init

---
+++ src/main/java/com/gustavomp/almacen/springbootmicroservicesalmaceneureka/SpringBootMicroservicesAlmacenEurekaApplication.java
@@ -0,0 +1,13 @@
+package com.gustavomp.almacen.springbootmicroservicesalmaceneureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMicroservicesAlmacenEurekaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootMicroservicesAlmacenEurekaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gustavomp/almacen/springbootmicroservicesalmaceneureka/SpringBootMicroservicesAlmacenEurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gustavomp.almacen.springbootmicroservicesalmaceneureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootMicroservicesAlmacenEurekaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
