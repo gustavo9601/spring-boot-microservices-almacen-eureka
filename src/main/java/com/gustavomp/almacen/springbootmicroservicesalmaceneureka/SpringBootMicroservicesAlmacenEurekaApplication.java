4465b57091a86ed929245f03925111d013ce185c
gustavo9601 <tavo9601@gmail.com>
2022-09-03 21:51:16 -0500

    allow eureka

--- src/main/java/com/gustavomp/almacen/springbootmicroservicesalmaceneureka/SpringBootMicroservicesAlmacenEurekaApplication.java
+++ src/main/java/com/gustavomp/almacen/springbootmicroservicesalmaceneureka/SpringBootMicroservicesAlmacenEurekaApplication.java
@@ -2,8 +2,10 @@ package com.gustavomp.almacen.springbootmicroservicesalmaceneureka;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer // Habilitando como servidor
 public class SpringBootMicroservicesAlmacenEurekaApplication {
 
 	public static void main(String[] args) {
